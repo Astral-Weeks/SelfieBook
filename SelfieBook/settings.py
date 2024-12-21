2ff88f68427facce485cf177fbbe84513601daff
AstralWeeks <144658645+Astral-Weeks@users.noreply.github.com>
2024-12-21 22:02:51 +0800

    Update settings.py

--- SelfieBook/settings.py
+++ SelfieBook/settings.py
@@ -128,15 +128,15 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Media Fiels Configuration
+# Media Fields Configuration
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media'
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID ')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_SIGNATURE_NAME = 's3v4'
 AWS_S3_REGION_NAME = 'ap-southeast-2'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL =  None
