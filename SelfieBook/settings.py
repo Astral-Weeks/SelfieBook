b42226dba1b8e03c376270a6ce3a5ce1fa410ba6
AstralWeeks <144658645+Astral-Weeks@users.noreply.github.com>
2024-12-21 18:02:27 +0800

    Update settings.py

--- SelfieBook/settings.py
+++ SelfieBook/settings.py
@@ -131,7 +131,16 @@ USE_TZ = True
 # Media Fiels Configuration
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/staticfiles/'
+MEDIA_URL = '/media/'
+
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_OBJECT_ACCESS_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_DEFAULT_ACL = os.environ.get("AWS_DEFAULT_ACL")
+
+AWS_LOCATION = "media"
 
 
 # Static files (CSS, JavaScript, Images)
